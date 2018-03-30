8ac31b89025367460fdcbe3279d480f4dad9c3f3
tinanwlin <tinanwlin@gmail.com>
2018-03-30 07:24:05 +0000

    Make order lists after paying

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ActiveRecord::Base
+
+  has_many :orders
+  
   has_secure_password
+
   validates :firstname, :lastname, :email, :password, presence: true
   validates :email, uniqueness: true
 end
